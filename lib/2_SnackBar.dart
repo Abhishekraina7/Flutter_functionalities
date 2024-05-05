25355e5d1d9de22591a3d229682b1b7c239b576c
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2024-05-05 13:33:58 +0530

    Snackbar learned and discussed.

---
+++ lib/2_SnackBar.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+// Todo: Here is the documentation for snackbar: https://docs.flutter.dev/cookbook/design/snackbars
+
+
+class Snackbar extends StatefulWidget {
+  static String id = "SnackBar";
+  const Snackbar({super.key});
+
+  @override
+  State<Snackbar> createState() => _SnackbarState();
+}
+
+class _SnackbarState extends State<Snackbar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: null,
+        title: const Center(child: Text("Snackbar")),
+      ),
+      body: Center(
+        child: ElevatedButton(onPressed: (){
+          const snackbar =   SnackBar(
+            duration: Duration(seconds: 3),
+            backgroundColor: Colors.red,
+              content:
+              Text("Boom here is the Snack Bar")
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackbar);
+        },
+            child: const Text("ShowSnackBar")),
+      ),
+    );
+  }
+}
