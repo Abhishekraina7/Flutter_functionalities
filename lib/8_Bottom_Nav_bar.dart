e85ebbadfc2f7f25a598b013dcaae91ff431dae1
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2024-05-07 17:29:14 +0530

    Added the Page controller which is used in real apps in production.

--- lib/8_Bottom_Nav_bar.dart
+++ lib/8_Bottom_Nav_bar.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_basics_one_short/2_SnackBar.dart';
+import 'package:flutter_basics_one_short/5_image_widget.dart';
+import 'package:flutter_basics_one_short/7_Bottom_sheet.dart';
+
+// todo: BottomNavBar doc--- https://api.flutter.dev/flutter/material/BottomNavigationBar-class.html
+// todo: PageController doc --  https://api.flutter.dev/flutter/widgets/PageController-class.html
 
 
 
@@ -12,42 +18,52 @@ class Bottom_nav_bar extends StatefulWidget {
 
 class _Bottom_nav_barState extends State<Bottom_nav_bar> {
   int selected_index = 0;
-  List<Widget> Items = <Widget>[
-    const Text("Home"),
-    const Text("Business"),
-    const Text("School"),
-  ];
+  PageController page_controller = PageController(); // this helps you to navigate through various pages in your app
+
+  // List<Widget> Items = <Widget>[
+  //   const Text("Home"),
+  //   const Text("Business"),
+  //   const Text("School"),
+  // ];
+
   void onTapped(int index){
     setState(() {
       selected_index = index;
     });
+    page_controller.jumpToPage(index);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        leading: null,
-        title:const Center(child: Text("BOTTOM NAV BAR"),),
+      // appBar: AppBar(
+      //   backgroundColor: Colors.green,
+      //   leading: null,
+      //   title:const Center(child: Text("BOTTOM NAV BAR"),),
+      // ),
+      body: PageView(  // This
+        controller: page_controller,
+        children: <Widget>[
+           Snackbar(),
+          Bottom_sheet(),
+          Image_widget(),
+        ],
       ),
-
-      body: Center(child: Items.elementAt(selected_index)),// displays the text present in the active index
 
       // Following part is not dynamic i.e you can't Interact with it
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Icon(Icons.bar_chart),
+            label: 'SnackBar',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: Icon(Icons.add),
+            label: 'BottomSheet',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(Icons.image),
+            label: 'Image',
           ),
         ],
         currentIndex: selected_index,
