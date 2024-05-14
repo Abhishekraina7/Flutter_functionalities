00b3e833de548594d1a30de89c453514961c06f0
Abhishek Raina <143702712+Abhishekraina7@users.noreply.github.com>
2024-05-14 23:26:17 +0530

    Added the required paths for different dart files for rendering different screen as the home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_basics_one_short/10_Forms.dart';
-import 'package:flutter_basics_one_short/11.%20Stack_n_Positioned.dart';
-import 'package:flutter_basics_one_short/12_Tab_Bar.dart';
 import 'package:flutter_basics_one_short/1_listview_n_grid_view.dart';
 import 'package:flutter_basics_one_short/2_SnackBar.dart';
 import 'package:flutter_basics_one_short/3_Dismissible.dart';
@@ -11,6 +8,11 @@ import 'package:flutter_basics_one_short/6_alert_dialog_box.dart';
 import 'package:flutter_basics_one_short/7_Bottom_sheet.dart';
 import 'package:flutter_basics_one_short/8_Bottom_Nav_bar.dart';
 import 'package:flutter_basics_one_short/9.Drop_down.dart';
+import 'package:flutter_basics_one_short/10_Forms.dart';
+import 'package:flutter_basics_one_short/11.%20Stack_n_Positioned.dart';
+import 'package:flutter_basics_one_short/12_Tab_Bar.dart';
+import 'package:flutter_basics_one_short/13_Image_picker.dart';
+import 'package:flutter_basics_one_short/14_Simple_insta_UI.dart';
 
 void main(){
   runApp(flutter_basics_one_short());
@@ -21,7 +23,7 @@ class flutter_basics_one_short extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
-      initialRoute: Tab_Bar.id,
+      initialRoute: Clone.id,
       routes: {
         List_view.id: (context) => const List_view(),
         Snackbar.id: (context) => const Snackbar(),
@@ -35,6 +37,8 @@ class flutter_basics_one_short extends StatelessWidget {
         forms_class.id: (context) => const forms_class(),
         Stack_n_positioned.id: (context) => const Stack_n_positioned(),
         Tab_Bar.id: (context) => const Tab_Bar(),
+        Image_picker.id: (context) => const Image_picker(),
+        Clone.id: (context) => const Clone(),
       },
     );
   }
